86d886d59ac144ed0fa96b35b35dcfd7a9e1ac6d
vijaysf123 <109033340+vijaysf123@users.noreply.github.com>
2022-07-26 18:03:28 +0530

    all the files are added

---
+++ script.js
@@ -0,0 +1,30 @@
+let menu = document.getElementById("menu-bars");
+let navbar = document.querySelector(".navbar")
+
+menu.addEventListener("click",function(){
+    menu.classList.toggle("fa-times")
+    navbar.classList.toggle("active")
+});
+
+document.querySelector('#search-icon').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active');
+}
+  
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active'); 
+}
+
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+  });
